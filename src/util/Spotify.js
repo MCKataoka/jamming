32eb8f91a77186099e4457f8e1c039ca9a2f059b
Mario C Kataoka <mario.c.kataoka@gmail.com>
2018-05-08 02:16:17 +0100

    latest version

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,4 @@
-let accessToken = '';
+let accessToken = null;
 let expiresIn = 0;
 const clientID = '0e744062a6a649659f46a354b2a06d9f';
 const redirectURI = "http://localhost:3000/";
@@ -7,20 +7,24 @@ const redirectURI = "http://localhost:3000/";
 
 let Spotify = {
   getAccessToken(){
-    console.log(accessToken)
-    if(accessToken !== ''){
+    if(accessToken){
       return accessToken;
+    }
+    accessToken = window.location.href.match(/access_token=([^&]*)/);
+    console.log(accessToken);
+    expiresIn = window.location.href.match(/expires_in=([^&]*)/);
+    if(accessToken && expiresIn){
+        accessToken = accessTokenMatch[1];
+        expiresIn = Number(expiresInMatch[1]);
+        window.setTimeout(() => accessToken = '', expiresIn * 1000);
+        window.history.pushState('Access Token', null, '/');
     }else{
-      let url =`https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
-      window.location = url;
-      accessToken = window.location.href.match(/access_token=([^&]*)/);
-      console.log(accessToken);
-      expiresIn = window.location.href.match(/expires_in=([^&]*)/);
-      window.setTimeout(() => accessToken = '', expiresIn * 1000);
-      window.history.pushState('Access Token', null, '/');
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+      window.location = accessUrl;
     }
   },
   search(term) {
+
       return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}` , {
       headers: {Authorization: `Bearer ${accessToken}`
       }
